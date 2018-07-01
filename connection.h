0776512f3cc0a9b71f52e57257b7a79b118e1355
paul17843 <paul@ska.ac.za>
2018-07-01 20:24:24 +0200

    adding Arduino project file and connection.h. Adding Arduino IDE settings to README.md

---
+++ connection.h
@@ -0,0 +1,11 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+// Security details for your wifi network here
+#define WIFI_SSID "wifi ssid"
+#define WIFI_PWD  "wifi password"
+
+// MQTT broker hostname
+#define MQTT_BROKER "broker.mqtt-dashboard.com"
+
+#endif /* CONNECTION_H */
